b7fb68b4ccc3a045095b9a5501c61c61759e3c65
Karan Babar <karan.babar2004@gmail.com>
2026-05-27 22:45:15 +0530

    Added event registration form with validation. Completed HTML Exercise 5

--- Module-1/local-community-event-portal/main.js
+++ Module-1/local-community-event-portal/main.js
@@ -3,3 +3,17 @@ console.log("Welcome to the Community Portal");
 window.onload = function () {
     alert("Page Loaded Successfully");
 };
+
+const registrationForm = document.getElementById("registrationForm");
+
+const confirmationMessage = document.getElementById("confirmationMessage");
+
+registrationForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const userName = document.getElementById("name").value;
+
+    confirmationMessage.textContent =
+        `Thank you ${userName}! Your registration has been submitted successfully.`;
+});
