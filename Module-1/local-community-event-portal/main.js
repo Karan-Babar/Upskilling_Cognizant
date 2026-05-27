137c162ee5b5506336e4bb28f9e380c6ffe835c7
Karan Babar <karan.babar2004@gmail.com>
2026-05-28 00:03:27 +0530

    Completed feedback form event handling. Completed HTML Exercise 6

--- Module-1/local-community-event-portal/main.js
+++ Module-1/local-community-event-portal/main.js
@@ -17,3 +17,50 @@ registrationForm.addEventListener("submit", function(event) {
     confirmationMessage.textContent =
         `Thank you ${userName}! Your registration has been submitted successfully.`;
 });
+
+function validatePhone() {
+
+    const phone = document.getElementById("phone").value;
+
+    const phoneError = document.getElementById("phoneError");
+
+    const phonePattern = /^[0-9]{10}$/;
+
+    if (!phonePattern.test(phone)) {
+        phoneError.textContent = "Please enter a valid 10-digit phone number.";
+    } else {
+        phoneError.textContent = "";
+    }
+}
+
+function showEventFee() {
+    const fee = document.getElementById("eventFee").value;
+
+    const feeDisplay = document.getElementById("feeDisplay");
+
+    if (fee !== "") {
+        feeDisplay.textContent =`Event Fee: ₹${fee}`;
+    } else {
+        feeDisplay.textContent = "";
+    }
+}
+
+function submitFeedback() {
+    alert("Feedback submitted successfully!");
+}
+
+function enlargeImage() {
+
+    const image = document.getElementById("eventImage");
+
+    image.style.width = "500px";
+}
+
+function countCharacters() {
+
+    const feedbackText = document.getElementById("feedbackText");
+
+    const charCount = document.getElementById("charCount");
+
+    charCount.textContent = feedbackText.value.length;
+}
